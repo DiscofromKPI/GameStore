292a646a75821683f243a847671c9aebdb433cb0
bohdan <bogdan2003bk96@gmail.com>
2021-11-30 18:01:24 +0200

    added role initializer

---
+++ GameStore/DAL/RoleInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using DAL.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace DAL
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            const string adminEmail = "admin@gmail.com";
+            const string password = "12345";
+            const string name = "admin";
+
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+            if (await roleManager.FindByNameAsync("manager") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("manager"));
+            }
+
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User() { Email = adminEmail, UserName = name };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                    await userManager.AddToRoleAsync(admin, "admin");
+            }
+        }
+    }
+}
